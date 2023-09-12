09eeae30732ac42bf4a3553cae9354efa85faa0b
Jhimy Ferrari <souzaferrarik@gmail.com>
2023-09-12 14:38:55 -0300

    config: add Colors and Font

---
+++ src/util/Colors.js
@@ -0,0 +1,12 @@
+export const Colors = {
+    backgroundLight: '#F9F9F9',
+    buttonPrimary:'#DB3022',
+    buttonSecondary:'#222222',
+    textField:'#ffffff',
+    textDark:'#222222',
+    textGray:'#9B9B9B',
+    textLight:'#ffffff',
+}
+
+
+
